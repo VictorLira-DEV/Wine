4dd96f5e69ec13e31fadf27e3d96bdff4d329205
VictorLira-DEV <victorliradev@gmail.com>
2022-04-08 03:32:34 -0300

    kkk

--- js/banner-slider.js
+++ js/banner-slider.js
@@ -1,30 +1,23 @@
 import { SLIDER_INITAL_POSITION } from './config.js';
+import { createDots, activeDots } from './slider.js';
 
 const slides = document.querySelectorAll('.header__slide');
 const btnLeft = document.querySelector('.header__button--left');
 const btnRight = document.querySelector('.header__button--right');
-const dotContainer = document.querySelector('.header__dots');
+const dotContainer = document.querySelector('.slider-dots');
 
 let currentSlide = SLIDER_INITAL_POSITION;
 const maxSlide = slides.length;
 
-const createDots = function () {
-    slides.forEach((_, i) => {
-        dotContainer.insertAdjacentHTML(
-            'beforeend',
-            `<button class="header__dot" data-slide="${i}"></button>`
-        );
-    });
-};
-const activeDots = function (slide) {
-    document.querySelectorAll('.header__dot').forEach(dot => {
-        dot.classList.remove('header__dot--active');
+// const activeDots = function (slide) {
+//     document.querySelectorAll('.header__dot').forEach(dot => {
+//         dot.classList.remove('header__dot--active');
 
-        document
-            .querySelector(`.header__dot[data-slide="${slide}"]`)
-            .classList.add('header__dot--active');
-    });
-};
+//         document
+//             .querySelector(`.header__dot[data-slide="${slide}"]`)
+//             .classList.add('header__dot--active');
+//     });
+// };
 
 const goToSlide = function (position) {
     slides.forEach((slide, index) => {
@@ -56,7 +49,7 @@ btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide);
 
 dotContainer.addEventListener('click', function (e) {
-    if (e.target.classList.contains('header__dot')) {
+    if (e.target.classList.contains('slider__dot')) {
         const { slide } = e.target.dataset;
         goToSlide(slide);
         activeDots(slide);
@@ -65,7 +58,7 @@ dotContainer.addEventListener('click', function (e) {
 
 const callBannerSlider = function () {
     goToSlide(SLIDER_INITAL_POSITION);
-    createDots();
+    createDots(slides, dotContainer)
     activeDots(SLIDER_INITAL_POSITION);
 };
 
